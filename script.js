93568f4fb2769aa7f76fe0f3cfaede5272aab0b8
Елена <elena@MacBook-Air-Elena-2.local>
2023-02-18 18:00:02 +0300

    Добавила даты

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function getData() {
   }
   
   async function main() {
-    const postsData = await getData();
+    const postsData = await getData();  //ТА САМАЯ НУЖНАЯ ПЕРЕМЕННАЯ С МАССИВОМ
     let currentPage = 1;
     let rows = 7;
   
@@ -21,10 +21,13 @@ async function getData() {
       paginatedData.forEach((el) => {
         const postEl = document.createElement("div");
         const postElBody = document.createElement("div");
+        const postDate = document.createElement("div");
         postEl.classList.add("post");
-        postEl.innerText = `${el.title}\r\n`;
+        postEl.innerText = "Тема:   " + `${el.title}\r\n`;
         postElBody.classList.add("post_body");
         postElBody.innerText = `${el.body}`;
+        postDate.innerText = getRandomDate();
+        postsEl.appendChild(postDate);
         postsEl.appendChild(postEl);
         postsEl.appendChild(postElBody);
       })
@@ -38,14 +41,14 @@ async function getData() {
   
       for (let i = 0; i < pagesCount; i++) {
         const liEl = displayPaginationBtn(i + 1);
-        ulEl.appendChild(liEl)
+        ulEl.appendChild(liEl);
       }
-      paginationEl.appendChild(ulEl)
+      paginationEl.appendChild(ulEl);
     }
   
     function displayPaginationBtn(page) {
       const liEl = document.createElement("li");
-      liEl.classList.add('pagination__item')
+      liEl.classList.add('pagination__item');
       liEl.innerText = page
   
       if (currentPage == page) liEl.classList.add('pagination__item--active');
@@ -69,3 +72,31 @@ async function getData() {
   
   main();
   
+
+
+  function getRandomDate() {
+    let start = new Date();
+    start.setDate(1);
+    start.setMonth(8);
+    start.setFullYear(2022);
+    let startTime = start.getTime();
+
+    let end = new Date();
+    end.setDate(11);
+    end.setMonth(1);
+    end.setFullYear(2022);
+    let endTime = end.getTime();
+    
+    let randomDate = new Date();
+    let randomDateTime = startTime + Math.random() * (endTime - startTime);
+    randomDate.setTime(randomDateTime);
+
+    let rndDate = randomDate.getDate();
+    let rndMonth = randomDate.getMonth() + 1;
+    let rndYear = randomDate.getFullYear();
+
+      return rndDate + " / " + rndMonth + " / " + rndYear;
+  }
+
+
+  console.log(getRandomDate());
